b4f3fbadf87cdb0813fd993ccdf6bafdcc9890a5
watagassa <k23095kk@aitech.ac.jp>
2024-07-21 23:28:21 +0900

    Reportのレイアウト作成

---
+++ src/components/MonthSelector.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MonthSelector = () => {
+  return (
+    <div>MonthSelector</div>
+  )
+}
+
+export default MonthSelector
--- src/pages/Report.tsx
+++ src/pages/Report.tsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import { Grid, Paper } from "@mui/material";
+import React from "react";
+import MonthSelector from "../components/MonthSelector";
+import CategoryChart from "../components/CategoryChart";
+import TransactionTable from "../components/TransactionTable";
+import BarChart from "../components/BarChart";
 
 const Report = () => {
-  return (
-    <div>Report</div>
-  )
-}
+  const commonPaperStyle = {
+    height: "400px",
+    // flexGrowを使用するためにflex
+    display: "flex",
+    //flexだが、縦並びにしたいのでcolumn
+    flexDirection: "column",
+    p: 2,
+  };
 
-export default Report
+  return (
+    <Grid container spacing={2}>
+      {/* xsはMUIが用意しているブレークポイント */}
+      <Grid item xs={12}>
+        {/* 日付選択エリア */}
+        <MonthSelector />
+      </Grid>
+
+      <Grid item xs={12} md={4}>
+        {/* MUIの Paperクラスは四角い紙を置いたようなデザイン*/}
+        <Paper sx={commonPaperStyle}>
+          {/* 円グラフ */}
+          <CategoryChart />
+        </Paper>
+      </Grid>
+
+      <Grid item xs={12} md={8}>
+        <Paper sx={commonPaperStyle}>
+          {/* 棒グラフ */}
+          <BarChart />
+        </Paper>
+      </Grid>
+
+      <Grid item xs={12}>
+        {/* テーブル */}
+        <TransactionTable />
+      </Grid>
+    </Grid>
+  );
+};
+
+export default Report;
